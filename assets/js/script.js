77fe5deda53f0a42097dbdda22b54918b435f1af
Carlos Mamani Ardiles <cmamani2003@gmail.com>
2024-08-21 01:40:02 -0400

    añadiendo funcionalidad navegador

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+
+const navToggle = document.querySelector(".navbar-toggle");
+navToggle.addEventListener("click", function () {
+  document.querySelector(".port-navbar").classList.toggle("show");
+});
